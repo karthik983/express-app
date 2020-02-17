11443770bc3e2bb84df5ea534e78d3a5d601a0a8
karthik983 <karthikh032@gmail.com>
2020-02-17 21:51:05 +0530

    login page

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require("express")
+var bodyParser = require("body-parser")
+const config = require("config")
+const fs = require("fs")
+const app = express();
+
+const port = process.env.PORT||config.get("PORT");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static("viewme"))
+
+app.post("/register", (req, res, next) => {
+    res.json({ "Success": "true" });
+    if(req.body.hob1==undefined && req.body.hob2==undefined && req.body.hob3==undefined){
+        req.body.hobbies="none"
+    }
+     else if(req.body.hob1==undefined && req.body.hob2!=undefined && req.body.hob3!=undefined){
+        req.body.hobbies=`${req.body.hob2} and ${req.body.hob3}`
+    }else if(req.body.hob2==undefined && req.body.hob1!=undefined && req.body.hob3!=undefined){
+        req.body.hobbies=`${req.body.hob1} and ${req.body.hob3}`
+    }else if(req.body.hob3==undefined && req.body.hob1!=undefined && req.body.hob2!=undefined){
+        req.body.hobbies=`${req.body.hob1} and ${req.body.hob2}`
+    }else if(req.body.hob1==undefined && req.body.hob2==undefined){
+        req.body.hobbies=`${req.body.hob3}`
+    }else if(req.body.hob2==undefined && req.body.hob3==undefined){
+        req.body.hobbies=`${req.body.hob1}`
+    }else if(req.body.hob1==undefined && req.body.hob3==undefined){
+        req.body.hobbies=`${req.body.hob2}`
+    }else{
+        req.body.hobbies=`${req.body.hob1} and ${req.body.hob2} and ${req.body.hob3}`
+    }
+    fs.writeFile("data.txt",
+        `First Name: ${req.body.fname}\nLast Name : ${req.body.lname}\nPassword:${req.body.pass}\nemail:${req.body.email}\nmobile:${req.body.mobile}\nAddress:${req.body.address}\nDOB:${req.body.dob}\nGender:${req.body.gender}\nHobbies:${req.body.hobbies}\nCountry:${req.body.country}`
+        , (err) => {
+            if (err) {
+                throw err;
+            } else {
+                console.log("Saved your file!")
+            }
+        })
+    })
+app.listen(port, () => {
+    console.log(`Server started at ${port}`);
+})
